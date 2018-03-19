c5bc485097b997bb5fbba2ddd0d107af28f8cb01
indigo-sam <sam.jin@hotmail.com>
2018-03-19 10:28:40 -0700

    .

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-import {firebaseApp} from './firebaseConfig'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import style from './scss/app.scss'
+import {firebaseApp} from './firebaseConfig'
 
 Vue.config.productionTip = false
 
 const storageRef = firebaseApp.storage().ref();
-// storageRef.child("").put(data).then(function(snapshot) {
-//     console.log();
-// });
+storageRef.child('desktop-images/20dollar.jpg').getDownloadURL().then(function(url) {
+    console.log(url);
+}).catch(function(error) {
+    console.warn(error);
+});
+
+
 new Vue({
   router,
   style,
